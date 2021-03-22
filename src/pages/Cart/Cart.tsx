25563629764df615e9da7e7c95bc4d3aac2a2617
Jesús Gallego <jesusgallego@outlook.com>
2021-03-22 16:46:55 +0100

    feat: go to Checkout only if the user is logged in

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { RefreshControl, ScrollView } from "react-native";
+import React, { useCallback } from "react";
+import { RefreshControl, ScrollView, Alert } from "react-native";
 import { NetworkStatus, useQuery } from "@apollo/client";
 
 import { CART } from "../../api/queries";
 import { Box, Button, Loading } from "../../components/atoms";
 import { CartProps } from "../../navigation/Cart";
 import { Cart as ICart } from "../../api/types/Cart";
+import { useAuth } from "../../components/organisms/AuthContext";
 
 import Summary from "./Components/Summary";
 import Row from "./Components/Row";
@@ -13,7 +14,30 @@ import CartPlaceholder from "./Components/CartPlaceholder";
 
 const Cart = ({ navigation }: CartProps) => {
   const { loading, data, refetch, networkStatus } = useQuery<ICart>(CART);
+  const { isAuthenticated } = useAuth();
   const isEmpty = (data?.cart?.products?.length ?? 0) === 0;
+
+  const toCheckout = useCallback(() => {
+    if (isAuthenticated) {
+      navigation.navigate("Checkout");
+    } else {
+      Alert.alert(
+        "Iniciar sesión",
+        "Para proceder con el pago es necesario que primero inicies sesión o te registres",
+        [
+          { text: "Cancelar", style: "cancel" },
+          {
+            text: "Iniciar sesión",
+            style: "default",
+            onPress: () => {
+              navigation.navigate("Login");
+            },
+          },
+        ]
+      );
+    }
+  }, [navigation, isAuthenticated]);
+
   if (loading && networkStatus !== NetworkStatus.refetch) {
     return <Loading />;
   }
@@ -36,7 +60,7 @@ const Cart = ({ navigation }: CartProps) => {
         </Box>
       </ScrollView>
       <Button
-        onPress={() => navigation.navigate("Checkout")}
+        onPress={toCheckout}
         margin="l"
         label={`Confirmar compra (${data?.cart?.total})`}
       />
