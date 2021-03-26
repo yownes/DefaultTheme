55cd67e8b28d881ed940329fd506641b9e1b7c12
Jesús Gallego <jesusgallego@outlook.com>
2021-03-26 16:57:21 +0100

    feat: select component

---
+++ src/components/molecules/Select.tsx
@@ -0,0 +1,114 @@
+import {
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  BottomSheetModalProvider,
+  BottomSheetScrollView,
+} from "@gorhom/bottom-sheet";
+import React, {
+  createContext,
+  Children,
+  useContext,
+  useRef,
+  useState,
+  useEffect,
+} from "react";
+import { TouchableOpacity } from "react-native";
+
+import { Box, Input, Text } from "../atoms";
+
+interface SelectProps {
+  children: React.ReactNode;
+  placeholder: string;
+  defaultValue?: string;
+  onChange: (value?: string | null) => void;
+}
+
+interface SelectItemProps {
+  id: string;
+  title: string;
+}
+
+const SelectContext = createContext<{
+  selected: string | undefined;
+  setSelected: (value: string) => void;
+}>({
+  selected: undefined,
+  setSelected: () => null,
+});
+
+const snapPoints = ["80%"];
+
+const Select = ({
+  children,
+  placeholder,
+  onChange,
+  defaultValue,
+}: SelectProps) => {
+  const modalRef = useRef<BottomSheetModal>(null);
+  const [selected, setSelected] = useState<string | null>(defaultValue || null);
+
+  useEffect(() => {
+    onChange(selected);
+  }, [onChange, selected]);
+
+  return (
+    <>
+      <TouchableOpacity
+        onPress={() => {
+          modalRef.current?.present();
+        }}
+      >
+        <Input value={selected || placeholder} pointerEvents="none" />
+      </TouchableOpacity>
+      <BottomSheetModal
+        index={0}
+        ref={modalRef}
+        snapPoints={snapPoints}
+        backdropComponent={BottomSheetBackdrop}
+      >
+        <BottomSheetScrollView>
+          <Box padding="l">
+            <Text variant="header" marginBottom="m">
+              {placeholder}
+            </Text>
+            <SelectContext.Provider value={{ selected, setSelected }}>
+              {Children.map(children, (child, index) => (
+                <PrivateItem
+                  {...(child.props as SelectItemProps)}
+                  key={index}
+                />
+              ))}
+            </SelectContext.Provider>
+          </Box>
+        </BottomSheetScrollView>
+      </BottomSheetModal>
+    </>
+  );
+};
+
+const PrivateItem = ({ id, title }: SelectItemProps) => {
+  const { setSelected, selected } = useContext(SelectContext);
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        setSelected(id);
+      }}
+    >
+      <Box
+        padding="m"
+        borderRadius={5}
+        backgroundColor={selected === id ? "greyscale2" : undefined}
+      >
+        <Text variant="header2">{title}</Text>
+      </Box>
+    </TouchableOpacity>
+  );
+};
+
+export const Item = (_props: SelectItemProps) => {
+  throw new Error("Child must be in Parent");
+};
+
+export const SelectProvider = BottomSheetModalProvider;
+
+export default Select;
--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -4,6 +4,11 @@ export { default as CreditCard } from "./CreditCard";
 export { default as HomeSlide } from "./HomeSlide";
 export { default as HorizontalScrollProducts } from "./HorizontalScrollProducts";
 export { default as InputWithErrors } from "./InputWithErrors";
+export {
+  default as Select,
+  Item as SelectItem,
+  SelectProvider,
+} from "./Select";
 export { default as Slider } from "./Slider";
 export { default as Placeholder } from "./Placeholder";
 export { default as ProductCard } from "./ProductCard";
