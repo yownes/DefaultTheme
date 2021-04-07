3874a84aa464593e9465b8ce9993b8b5ba1e8174
Jesús Gallego <jesusgallego@outlook.com>
2021-04-07 10:13:40 +0200

    test: extract token from cookies

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,10 +14,15 @@ export function removeToken() {
   return SecureStore.deleteItemAsync(TOKEN_KEY);
 }
 
-export function extractTokenFromHeaders(headers: Record<string, any>) {
+export function extractTokenFromHeaders(headers: Map<string, string>) {
   const setCookie = headers.get("set-cookie");
 
   const regex = /PrestaShop-[a-zA-Z0-9]*=[^;]{2,}/gm;
+
+  if (!setCookie) {
+    return;
+  }
+
   let result;
   //obtain last cookie
   let match = regex.exec(setCookie);
