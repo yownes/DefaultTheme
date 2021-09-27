399dc7c2b9b29f9fbbdb8eb8a3bc1bd930beb8d1
Jesús Gallego <jesusgallego@outlook.com>
2021-09-27 15:48:40 +0200

    feat: :sparkles: favourites placeholder

--- src/pages/Profile/Favourites.tsx
+++ src/pages/Profile/Favourites.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import { FlatList } from "react-native";
 import { useQuery } from "@apollo/client";
 
-import { Box } from "../../components/atoms";
+import { Box, Text } from "../../components/atoms";
 import { FAVOURITES } from "../../api/queries";
 import { Favourites as IFavourites } from "../../api/types/Favourites";
 import { ProductCard } from "../../components/molecules";
+import NoFavsImage from "../../components/images/NoFavs";
+
+const FavouritesPlaceholder = () => (
+  <Box padding="l">
+    <NoFavsImage />
+    <Text variant="header2" textAlign="center" marginVertical="l">
+      No tienes productos favoritos
+    </Text>
+  </Box>
+);
 
 const Favourites = () => {
   const { data } = useQuery<IFavourites>(FAVOURITES);
@@ -14,6 +24,7 @@ const Favourites = () => {
       <FlatList
         data={data?.wishlist}
         keyExtractor={(item) => item?.id}
+        ListEmptyComponent={<FavouritesPlaceholder />}
         renderItem={({ item }) => (
           <Box marginBottom="m" key={item?.id}>
             <ProductCard product={item} />
