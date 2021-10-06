133d76b3b84bc64ee19a190151a387dd6a6635b7
Jesús Gallego <jesusgallego@outlook.com>
2021-10-06 15:29:08 +0200

    refactor: :recycle: apollo provider and client from @yownes/api

--- App.tsx
+++ App.tsx
@@ -3,20 +3,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ThemeProvider } from "@shopify/restyle";
-import { ApolloProvider } from "@apollo/client";
 import Constants from "expo-constants";
 import { StripeProvider } from "@stripe/stripe-react-native";
+import { ApiProvider } from "@yownes/api";
 
 import Root from "./src/navigation/Root";
 import theme from "./src/lib/theme";
-import client from "./src/lib/apollo-client";
 import { AuthProvider } from "./src/components/organisms/AuthContext";
+
+const uri = `${Constants.manifest.extra.apiUrl}/module/yownes/graphql`;
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <ApolloProvider client={client}>
+        <ApiProvider uri={uri}>
           <AuthProvider>
             <StripeProvider
               publishableKey={Constants.manifest?.extra?.stripeKey}
@@ -27,7 +28,7 @@ const App = () => {
               </NavigationContainer>
             </StripeProvider>
           </AuthProvider>
-        </ApolloProvider>
+        </ApiProvider>
       </ThemeProvider>
     </SafeAreaProvider>
   );
